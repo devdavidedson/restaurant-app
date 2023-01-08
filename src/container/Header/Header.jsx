be9f44126808885b64363baf75222a023d29428f
David Lopes <104918465+devdavidedson@users.noreply.github.com>
2023-01-08 13:48:14 -0300

    Aboutus complete

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => (
     <div className='app__wrapper_info'>
       <SubHeading title="Chase the new flavour"/>
       <h1 className='app__header-h1'>The Key to fine Dinning</h1>
-      <p className='p__opensas' style={{margin: '2rem 0'}}>Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus </p>
+      <p className='p__opensas'X style={{margin: '2rem 0', color: '#fff'}}>Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus </p>
       <button type="button" className="custom__button">Explore Menu</button>
     </div>
 
--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
-
+import { images } from '../..//constants';
+import { SubHeading, MenuItem } from '../../components';
 import './SpecialMenu.css';
 
 const SpecialMenu = () => (
-  <div>
-    SpecialMenu
+  <div className='app__specialMenu flex__center section__padding' id='menu'>
+    <div className='app__specialMenu-title'>
+      <SubHeading title='Menu that fits you palatte' />
+      <h1 className='headtext__cormorant'>Today's Special</h1>
+    </div>
+
+    <div></div>
   </div>
 );
 
